8343371e982be7d2532a338c0cd4a61a730fda6b
Vladimir Solovyov <leilyngale@icloud.com>
2023-01-16 09:12:55 +0800

    Type annotations in TS

--- grider/features/annotations/variables.ts
+++ grider/features/annotations/variables.ts
@@ -12,4 +12,12 @@ class Car{}
 let car: Car = new Car();
 
 // Object literal
-let point: 
+let point: { x: number, y: number } = {
+  x: 10,
+  y: 20
+}
+
+// functions
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+}
