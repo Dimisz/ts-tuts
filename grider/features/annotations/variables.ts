eef72f8e2a072f84b01cc95046738067a1fc8716
Vladimir Solovyov <leilyngale@icloud.com>
2023-01-16 08:53:47 +0800

    More on types

---
+++ grider/features/annotations/variables.ts
@@ -0,0 +1,15 @@
+const apples: number = 5;
+
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let nothing: null = null;
+
+let colors: string[] = ['red', 'green', 'blue'];
+
+// classes
+class Car{}
+let car: Car = new Car();
+
+// Object literal
+let point: 
